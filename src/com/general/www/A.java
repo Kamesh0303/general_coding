7c544691a036202e51911abc9dca51f23057ec4e
Kamesh0303 <kamesh.sh03@gmail.com>
2023-08-09 12:42:07 +0530

    first commit

---
+++ src/com/general/www/A.java
@@ -0,0 +1,17 @@
+package com.general.www;
+
+public class A {
+
+	public static void main(String[] args) {
+
+        int n= 4;
+        
+        for(int i=0; i<=n;i++)
+        {
+       	 System.out.println("Hello Worlds");
+        }
+        
+	
+
+}
+}
